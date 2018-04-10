f902e4b97ab9649915912674f939f8167d127b3a
Mungrel <nick.pollett@outlook.com>
2018-04-10 18:45:27 +1200

    Add ListChangeListener stub

---
+++ app/src/main/java/kres/realtimeshoppinglist/firebase/ListChangeListener.java
@@ -0,0 +1,42 @@
+package kres.realtimeshoppinglist.firebase;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListChangeListener implements ChildEventListener {
+
+    private List<String> keys;
+
+    public ListChangeListener() {
+        this.keys = new ArrayList<>();
+    }
+
+    @Override
+    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+    }
+
+    @Override
+    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+    }
+
+    @Override
+    public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+    }
+
+    @Override
+    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+    }
+
+    @Override
+    public void onCancelled(DatabaseError databaseError) {
+
+    }
+}
