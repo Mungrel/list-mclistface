6cf5befb44b6024a1b4fad92810c44ac56f902d3
Mungrel <nick.pollett@outlook.com>
2018-04-11 20:07:34 +1200

    Add transition logic from join activity to list activity

--- app/src/main/java/kres/realtimeshoppinglist/activities/JoinActivity.java
+++ app/src/main/java/kres/realtimeshoppinglist/activities/JoinActivity.java
@@ -1,7 +1,11 @@
 package kres.realtimeshoppinglist.activities;
 
+        import android.content.Intent;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
 
         import kres.realtimeshoppinglist.R;
 
@@ -11,5 +15,22 @@ public class JoinActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+
+        final EditText listCodeEditText = findViewById(R.id.list_code_edit_text);
+        Button submitButton = findViewById(R.id.submit_code_button);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String code = listCodeEditText.getText().toString();
+                if (code.isEmpty()) {
+                    return;
+                }
+
+                // TODO: Make some Firebase call to check list's existence
+                Intent intent = new Intent(JoinActivity.this, ListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
