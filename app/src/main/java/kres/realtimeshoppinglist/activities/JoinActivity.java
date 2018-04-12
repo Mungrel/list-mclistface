7604d2424b7aefed922a0b5f54684f076014bf6a
Mungrel <nick.pollett@outlook.com>
2018-04-12 18:33:55 +1200

    Fix import indents

--- app/src/main/java/kres/realtimeshoppinglist/activities/JoinActivity.java
+++ app/src/main/java/kres/realtimeshoppinglist/activities/JoinActivity.java
@@ -1,13 +1,13 @@
 package kres.realtimeshoppinglist.activities;
 
-        import android.content.Intent;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.EditText;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-        import kres.realtimeshoppinglist.R;
+import kres.realtimeshoppinglist.R;
 
 public class JoinActivity extends AppCompatActivity {
 
