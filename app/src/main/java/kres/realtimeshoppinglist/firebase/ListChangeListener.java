57623f24f86e4288f8c2d75de814d8c7f42a5901
Nick <nick.pollett@vendhq.com>
2018-04-12 20:19:42 +1200

    Implement ListChangeListener (#15)
    * Add ProductListAdapter field
    
    * Implement onChildAdded
    
    * Implement onChildChanged
    
    * Implement onChildRemoved
    
    * Implement onChildMoved

--- app/src/main/java/kres/realtimeshoppinglist/firebase/ListChangeListener.java
+++ app/src/main/java/kres/realtimeshoppinglist/firebase/ListChangeListener.java
@@ -9,32 +9,66 @@ import com.google.firebase.database.DatabaseError;
 import java.util.ArrayList;
 import java.util.List;
 
+import kres.realtimeshoppinglist.model.Product;
+import kres.realtimeshoppinglist.util.ProductListAdapter;
+
 public class ListChangeListener implements ChildEventListener {
 
     private List<String> keys;
+    private ProductListAdapter productListAdapter;
 
-    public ListChangeListener() {
+    public ListChangeListener(ProductListAdapter productListAdapter) {
         this.keys = new ArrayList<>();
+        this.productListAdapter = productListAdapter;
     }
 
     @Override
-    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+    public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
         Log.d("LIST_CHANGE", "Child Added");
+
+        Product product = dataSnapshot.getValue(Product.class);
+        product.setID(dataSnapshot.getKey());
+
+        int insertionIndex = (previousChildName == null) ? 0 : keys.indexOf(previousChildName) + 1;
+        keys.add(insertionIndex, dataSnapshot.getKey());
+
+        productListAdapter.insertItem(insertionIndex, product);
     }
 
     @Override
-    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+    public void onChildChanged(DataSnapshot dataSnapshot, String previousChildName) {
         Log.d("LIST_CHANGE", "Child Added");
+
+        Product updatedProduct = dataSnapshot.getValue(Product.class);
+
+        int insertionIndex = (previousChildName == null) ? 0 : keys.indexOf(previousChildName) + 1;
+
+        productListAdapter.updateItem(insertionIndex, updatedProduct);
     }
 
     @Override
     public void onChildRemoved(DataSnapshot dataSnapshot) {
         Log.d("LIST_CHANGE", "Child Added");
+
+        int removalIndex = keys.indexOf(dataSnapshot.getKey());
+        keys.remove(removalIndex);
+
+        productListAdapter.removeItem(removalIndex);
     }
 
     @Override
-    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+    public void onChildMoved(DataSnapshot dataSnapshot, String previousChildName) {
         Log.d("LIST_CHANGE", "Child Added");
+
+        String movedTrackKey = dataSnapshot.getKey();
+
+        int oldIndex = keys.indexOf(movedTrackKey);
+        keys.remove(oldIndex);
+        int newIndex = (previousChildName == null) ? 0 : keys.indexOf(previousChildName) + 1;
+
+        keys.add(newIndex, movedTrackKey);
+
+        productListAdapter.moveItem(oldIndex, newIndex);
     }
 
     @Override
