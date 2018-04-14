c0e734b89c77ace6042ac82ea5a63d0fcb11f307
Mungrel <nick.pollett@outlook.com>
2018-04-14 16:21:55 +1200

    Add empty list selection activity

---
+++ app/src/main/java/kres/realtimeshoppinglist/ListSelectActivity.java
@@ -0,0 +1,13 @@
+package kres.realtimeshoppinglist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_select);
+    }
+}
