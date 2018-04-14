80bee88dd6fb273e37386695294696831cd16c02
Mungrel <nick.pollett@outlook.com>
2018-04-14 16:05:32 +1200

    Add ListExistsListener

---
+++ app/src/main/java/kres/realtimeshoppinglist/firebase/shoppingList/ListExistsListener.java
@@ -0,0 +1,10 @@
+package kres.realtimeshoppinglist.firebase.shoppingList;
+
+import kres.realtimeshoppinglist.model.ShoppingList;
+
+public interface ListExistsListener {
+
+    void onListFound(ShoppingList list);
+    void onListNotFound();
+
+}
