71dbcfe2861d35585e4b515ea302cd717d547313
Mungrel <nick.pollett@outlook.com>
2018-04-14 16:11:20 +1200

    Log on list not found

--- app/src/main/java/kres/realtimeshoppinglist/activities/JoinActivity.java
+++ app/src/main/java/kres/realtimeshoppinglist/activities/JoinActivity.java
@@ -3,6 +3,7 @@ package kres.realtimeshoppinglist.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -28,7 +29,7 @@ public class JoinActivity extends AppCompatActivity {
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String code = listCodeEditText.getText().toString();
+                final String code = listCodeEditText.getText().toString();
                 if (code.isEmpty()) {
                     return;
                 }
@@ -47,7 +48,7 @@ public class JoinActivity extends AppCompatActivity {
 
                     @Override
                     public void onListNotFound() {
-                        
+                        Log.d("LIST_NOT_FOUND", "No list found with ID: " + code);
                     }
                 });
 
