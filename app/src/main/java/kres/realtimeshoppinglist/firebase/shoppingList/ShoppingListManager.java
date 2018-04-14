b94ee79a93951d1c0101b1d367d2cdd4fd8aaf55
Mungrel <nick.pollett@outlook.com>
2018-04-14 16:08:58 +1200

    Add getShoppingList method

--- app/src/main/java/kres/realtimeshoppinglist/firebase/shoppingList/ShoppingListManager.java
+++ app/src/main/java/kres/realtimeshoppinglist/firebase/shoppingList/ShoppingListManager.java
@@ -1,6 +1,11 @@
 package kres.realtimeshoppinglist.firebase.shoppingList;
 
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 import kres.realtimeshoppinglist.firebase.FirebaseRefs;
 import kres.realtimeshoppinglist.model.ShoppingList;
@@ -26,5 +31,25 @@ public class ShoppingListManager {
         DatabaseReference listRef = FirebaseRefs.getShoppingListRef(listID);
         DatabaseReference listNameRef = listRef.child("name");
         listNameRef.setValue(newListName);
+    }
+
+    public static void getShoppingList(String listID, final ListExistsListener listExistsListener) {
+        DatabaseReference listRef = FirebaseRefs.getShoppingListRef(listID);
+        listRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    ShoppingList list = dataSnapshot.getValue(ShoppingList.class);
+                    listExistsListener.onListFound(list);
+                } else {
+                    listExistsListener.onListNotFound();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d("LIST_FIND", "Error finding list: " + databaseError.getMessage());
+            }
+        });
     }
 }
