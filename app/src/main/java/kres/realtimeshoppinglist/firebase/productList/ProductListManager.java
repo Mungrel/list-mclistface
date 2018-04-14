baa4c14df2833511047274d7c94cf73292be24b6
Mungrel <nick.pollett@outlook.com>
2018-04-14 15:36:50 +1200

    Make methods in ProductListManager static

--- app/src/main/java/kres/realtimeshoppinglist/firebase/productList/ProductListManager.java
+++ app/src/main/java/kres/realtimeshoppinglist/firebase/productList/ProductListManager.java
@@ -7,14 +7,14 @@ import kres.realtimeshoppinglist.model.Product;
 
 public class ProductListManager {
 
-    public void addItem(String listID, Product product) {
+    public static void addItem(String listID, Product product) {
         DatabaseReference ref = FirebaseRefs.getShoppingListRef(listID);
         DatabaseReference newProductRef = ref.push();
         product.setID(newProductRef.getKey());
         newProductRef.setValue(product);
     }
 
-    public void editItem(String listID, Product updatedProduct) {
+    public static void editItem(String listID, Product updatedProduct) {
         if (updatedProduct.getID() == null || updatedProduct.getID().isEmpty()) {
             return;
         }
@@ -23,7 +23,7 @@ public class ProductListManager {
         productRef.setValue(updatedProduct);
     }
 
-    public void removeItem(String listID, String productID) {
+    public static void removeItem(String listID, String productID) {
         if (productID == null || productID.isEmpty()) {
             return;
         }
