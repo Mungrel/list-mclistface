4eb825c27fe7083106307194aa214bb9e193eea6
Mungrel <nick.pollett@outlook.com>
2018-04-14 15:13:31 +1200

    Add createShoppingList method

--- app/src/main/java/kres/realtimeshoppinglist/firebase/ShoppingListManager.java
+++ app/src/main/java/kres/realtimeshoppinglist/firebase/ShoppingListManager.java
@@ -1,5 +1,19 @@
 package kres.realtimeshoppinglist.firebase;
 
+import com.google.firebase.database.DatabaseReference;
+
+import kres.realtimeshoppinglist.model.ShoppingList;
+
 public class ShoppingListManager {
+
+    public ShoppingList createShoppingList(String listName) {
+        DatabaseReference newListRef = FirebaseRefs.getShoppingListRootRef().push();
+        String listID = newListRef.getKey();
+
+        ShoppingList shoppingList = new ShoppingList(listID, listName);
+        newListRef.setValue(shoppingList);
+
+        return shoppingList;
+    }
 
 }
