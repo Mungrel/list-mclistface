065f997cf178179e7fea26f0dc2f69f9cb5df664
Nick <nick.pollett@vendhq.com>
2018-04-14 15:27:06 +1200

    Shopping list name change listener (#17)
    * Rename ListChangeListener to more descriptive ProductListChangeListener
    
    * Add ShoppingListNameChangeListener class stub

---
+++ app/src/main/java/kres/realtimeshoppinglist/firebase/ShoppingListNameChangeListener.java
@@ -0,0 +1,19 @@
+package kres.realtimeshoppinglist.firebase;
+
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
+public class ShoppingListNameChangeListener implements ValueEventListener {
+    @Override
+    public void onDataChange(DataSnapshot dataSnapshot) {
+        Log.d("LIST_NAME_CHANGE", "List name changed");
+    }
+
+    @Override
+    public void onCancelled(DatabaseError databaseError) {
+        Log.d("LIST_NAME_ERROR", "" + databaseError.getMessage());
+    }
+}
