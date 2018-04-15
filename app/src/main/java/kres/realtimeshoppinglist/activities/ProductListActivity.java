14559e801616184a84ef949aca64d9ab26dd8ab8
Nick <nick.pollett@vendhq.com>
2018-04-15 21:18:11 +1200

    Product list activity logic (#26)
    * Add change listener to ref
    
    * Add dummy fab click listener
    
    * Add product to list on dialog end
    
    * Fix db ref

--- app/src/main/java/kres/realtimeshoppinglist/activities/ProductListActivity.java
+++ app/src/main/java/kres/realtimeshoppinglist/activities/ProductListActivity.java
@@ -6,23 +6,49 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.gson.Gson;
 
 import kres.realtimeshoppinglist.R;
+import kres.realtimeshoppinglist.firebase.FirebaseRefs;
+import kres.realtimeshoppinglist.firebase.productList.ProductListChangeListener;
+import kres.realtimeshoppinglist.firebase.productList.ProductListManager;
+import kres.realtimeshoppinglist.model.ShoppingList;
+import kres.realtimeshoppinglist.util.Constants;
+import kres.realtimeshoppinglist.util.ProductListAdapter;
+
 import kres.realtimeshoppinglist.dialog.NewProductDialog;
 import kres.realtimeshoppinglist.dialog.NewProductDialogListener;
 import kres.realtimeshoppinglist.model.Product;
 
 public class ProductListActivity extends AppCompatActivity implements NewProductDialogListener {
 
+    private String listID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_list);
 
+        LinearLayout productListLayout = findViewById(R.id.product_list_layout);
+        ProductListAdapter adapter = new ProductListAdapter(productListLayout, ProductListActivity.this);
+        ProductListChangeListener changeListener = new ProductListChangeListener(adapter);
+
+        String shoppingListJSON = getIntent().getStringExtra(Constants.SHOPPING_LIST_INTENT_KEY);
+        ShoppingList shoppingList = new Gson().fromJson(shoppingListJSON, ShoppingList.class);
+
+        listID = shoppingList.getId();
+
+        DatabaseReference listRef = FirebaseRefs.getShoppingListProductsRef(listID);
+        listRef.addChildEventListener(changeListener); 
+
         FloatingActionButton fab = findViewById(R.id.product_list_fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.d("PRODUCT_LIST_ACTIVITY", "FAB clicked");
                 DialogFragment fragment = new NewProductDialog();
                 fragment.show(getFragmentManager(), "New Product");
             }
@@ -32,10 +58,10 @@ public class ProductListActivity extends AppCompatActivity implements NewProduct
     @Override
     public void onProductAdded(Product product) {
         Log.d("PRODUCT_ADDED", "" + product.getName());
+        ProductListManager.addItem(listID, product);
     }
 
     @Override
     public void onCancel() {
-
     }
 }
