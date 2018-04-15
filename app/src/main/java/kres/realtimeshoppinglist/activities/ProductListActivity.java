121d2102d596fa6ebbc3f987f6cbd00135a501b1
Mungrel <nick.pollett@outlook.com>
2018-04-15 16:17:00 +1200

    Add change listener to ref

--- app/src/main/java/kres/realtimeshoppinglist/activities/ProductListActivity.java
+++ app/src/main/java/kres/realtimeshoppinglist/activities/ProductListActivity.java
@@ -2,8 +2,17 @@ package kres.realtimeshoppinglist.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.gson.Gson;
 
 import kres.realtimeshoppinglist.R;
+import kres.realtimeshoppinglist.firebase.FirebaseRefs;
+import kres.realtimeshoppinglist.firebase.productList.ProductListChangeListener;
+import kres.realtimeshoppinglist.model.ShoppingList;
+import kres.realtimeshoppinglist.util.Constants;
+import kres.realtimeshoppinglist.util.ProductListAdapter;
 
 public class ProductListActivity extends AppCompatActivity {
 
@@ -11,5 +20,15 @@ public class ProductListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_list);
+
+        LinearLayout productListLayout = findViewById(R.id.product_list_layout);
+        ProductListAdapter adapter = new ProductListAdapter(productListLayout, ProductListActivity.this);
+        ProductListChangeListener changeListener = new ProductListChangeListener(adapter);
+
+        String shoppingListJSON = getIntent().getStringExtra(Constants.SHOPPING_LIST_INTENT_KEY);
+        ShoppingList shoppingList = new Gson().fromJson(shoppingListJSON, ShoppingList.class);
+
+        DatabaseReference listRef = FirebaseRefs.getShoppingListRef(shoppingList.getId());
+        listRef.addChildEventListener(changeListener);
     }
 }
