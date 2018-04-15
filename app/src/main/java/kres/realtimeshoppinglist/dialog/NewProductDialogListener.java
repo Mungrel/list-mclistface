6dfde866bd6c699d0a18900ac14bf6e4668bc275
Mungrel <nick.pollett@outlook.com>
2018-04-15 17:11:50 +1200

    Add NewProductDialogListener

---
+++ app/src/main/java/kres/realtimeshoppinglist/dialog/NewProductDialogListener.java
@@ -0,0 +1,10 @@
+package kres.realtimeshoppinglist.dialog;
+
+import kres.realtimeshoppinglist.model.Product;
+
+public interface NewProductDialogListener {
+
+    void onProductAdded(Product product);
+    void onCancel();
+    
+}
