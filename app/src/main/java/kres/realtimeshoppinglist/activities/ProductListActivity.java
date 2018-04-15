a7c5027a785474bf7a263a4bbf61b5a6a88ce850
Nick <nick.pollett@vendhq.com>
2018-04-15 17:49:59 +1200

    New product dialog fragment (#25)
    * Add NewProduct dialog
    
    * Add NewProductDialogListener
    
    * Instantiate interface
    
    * Call interface methods onClicks
    
    * Show dialog on fab click
    
    * Set number picker values
    
    * Set quantity string

--- app/src/main/java/kres/realtimeshoppinglist/activities/ProductListActivity.java
+++ app/src/main/java/kres/realtimeshoppinglist/activities/ProductListActivity.java
@@ -1,15 +1,41 @@
 package kres.realtimeshoppinglist.activities;
 
+import android.app.DialogFragment;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 import kres.realtimeshoppinglist.R;
+import kres.realtimeshoppinglist.dialog.NewProductDialog;
+import kres.realtimeshoppinglist.dialog.NewProductDialogListener;
+import kres.realtimeshoppinglist.model.Product;
 
-public class ProductListActivity extends AppCompatActivity {
+public class ProductListActivity extends AppCompatActivity implements NewProductDialogListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_list);
+
+        FloatingActionButton fab = findViewById(R.id.product_list_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment fragment = new NewProductDialog();
+                fragment.show(getFragmentManager(), "New Product");
+            }
+        });
+    }
+
+    @Override
+    public void onProductAdded(Product product) {
+        Log.d("PRODUCT_ADDED", "" + product.getName());
+    }
+
+    @Override
+    public void onCancel() {
+
     }
 }
