ac35f0cdabdb693ab2a8600e9f0d01ffc31d86ae
Mungrel <nick.pollett@outlook.com>
2018-04-22 21:16:03 +1200

    Implement addItem

---
+++ app/src/main/java/kres/realtimeshoppinglist/firebase/shoppingList/ShoppingListAdapter.java
@@ -0,0 +1,41 @@
+package kres.realtimeshoppinglist.firebase.shoppingList;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import kres.realtimeshoppinglist.R;
+import kres.realtimeshoppinglist.model.ShoppingList;
+
+public class ShoppingListAdapter {
+
+    private LinearLayout listLayout;
+    private LayoutInflater inflater;
+
+    public ShoppingListAdapter(LinearLayout listLayout, Context context) {
+        this.listLayout = listLayout;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    public void appendItem(ShoppingList shoppingList) {
+        RelativeLayout newItemLayout = (RelativeLayout) inflater.inflate(R.layout.shopping_list_item_layout, null, false);
+
+        TextView listName = newItemLayout.findViewById(R.id.list_select_list_name);
+        ImageButton deleteButton = newItemLayout.findViewById(R.id.delete_list_button);
+
+        listName.setText(shoppingList.getName());
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Delete button clicked
+            }
+        });
+
+        listLayout.addView(newItemLayout);
+    }
+}
