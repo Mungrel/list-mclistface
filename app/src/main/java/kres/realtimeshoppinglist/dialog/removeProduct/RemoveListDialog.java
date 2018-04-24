008cf6efb79ec6f4be646a800d2e47aa32203610
Mungrel <nick.pollett@outlook.com>
2018-04-24 18:32:03 +1200

    Add RemoveListDialog

---
+++ app/src/main/java/kres/realtimeshoppinglist/dialog/removeProduct/RemoveListDialog.java
@@ -0,0 +1,56 @@
+package kres.realtimeshoppinglist.dialog.removeProduct;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+import kres.realtimeshoppinglist.firebase.productList.ProductListManager;
+import kres.realtimeshoppinglist.firebase.shoppingList.ShoppingListAdapter;
+import kres.realtimeshoppinglist.firebase.shoppingList.ShoppingListManager;
+
+public class RemoveListDialog {
+
+    private Context context;
+    private String listID;
+
+    private ShoppingListAdapter adapter;
+
+    private AlertDialog dialog;
+
+    public RemoveListDialog(Context context, String listID, ShoppingListAdapter adapter) {
+        this.context = context;
+        this.listID = listID;
+
+        this.adapter = adapter;
+
+        this.dialog = build();
+    }
+
+    private AlertDialog build() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+        builder.setTitle("Delete List");
+        builder.setMessage("Delete this list for everyone?");
+
+        builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                ShoppingListManager.deleteList(listID);
+                adapter.deleteItem(listID);
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                return;
+            }
+        });
+
+        return builder.create();
+    }
+
+    public void show() {
+        dialog.show();
+    }
+}
