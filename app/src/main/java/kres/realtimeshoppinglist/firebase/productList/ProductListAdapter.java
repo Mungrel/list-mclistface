594b99fa4ab409c2328181f99152d46bfe615977
Mungrel <nick.pollett@outlook.com>
2018-04-24 18:49:59 +1200

    Delete item on button click

--- app/src/main/java/kres/realtimeshoppinglist/firebase/productList/ProductListAdapter.java
+++ app/src/main/java/kres/realtimeshoppinglist/firebase/productList/ProductListAdapter.java
@@ -3,9 +3,12 @@ package kres.realtimeshoppinglist.firebase.productList;
 import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import kres.realtimeshoppinglist.R;
 import kres.realtimeshoppinglist.model.Product;
@@ -41,7 +44,7 @@ public class ProductListAdapter {
     }
 
     public void insertItem(int index, final Product item) {
-        LinearLayout listItem = (LinearLayout) inflater.inflate(R.layout.product_list_item_layout, null, false);
+        RelativeLayout listItem = (RelativeLayout) inflater.inflate(R.layout.product_list_item_layout, null, false);
         CheckBox itemCheckBox = listItem.findViewById(R.id.list_item_check_box);
 
         itemCheckBox.setText(item.getName());
@@ -52,6 +55,14 @@ public class ProductListAdapter {
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
                 item.setBought(isChecked);
                 ProductListManager.editItem(listID, item);
+            }
+        });
+
+        ImageButton deleteButton = listItem.findViewById(R.id.delete_product_button);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ProductListManager.removeItem(listID, item.getID());
             }
         });
 
