af5c83967ba49837aa757a5037ac341c5d6c113b
Mungrel <nick.pollett@outlook.com>
2018-04-24 17:54:19 +1200

    Create or add list on tab dialog 'add' click

--- app/src/main/java/kres/realtimeshoppinglist/dialog/JoinCreateDialog.java
+++ app/src/main/java/kres/realtimeshoppinglist/dialog/JoinCreateDialog.java
@@ -12,23 +12,31 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TabHost;
 
+import com.google.gson.Gson;
+
 import kres.realtimeshoppinglist.R;
+import kres.realtimeshoppinglist.firebase.shoppingList.ListExistsListener;
 import kres.realtimeshoppinglist.firebase.shoppingList.ShoppingListManager;
 import kres.realtimeshoppinglist.model.ShoppingList;
+import kres.realtimeshoppinglist.persistence.PersistenceManager;
 
 public class JoinCreateDialog extends DialogFragment {
 
+    private ShoppingListUtil utils;
+
     private static final String TAB_CREATE = "TAB_CREATE";
     private static final String TAB_JOIN = "TAB_JOIN";
+    private static final int TAB_CREATE_INDEX = 0;
+    private static final int TAB_JOIN_INDEX = 1;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
 
         View dialogView = inflater.inflate(R.layout.join_create_dialog, null);
 
-        TabHost host = dialogView.findViewById(R.id.tab_host);
+        final TabHost host = dialogView.findViewById(R.id.tab_host);
         host.setup();
 
         //Tab 1
@@ -52,7 +60,27 @@ public class JoinCreateDialog extends DialogFragment {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 Log.d("NEW_LIST_DIALOG", "Positive button clicked");
+                switch(host.getCurrentTab()) {
+                    case TAB_CREATE_INDEX:
+                        String newListName = listNameEditText.getText().toString();
+                        ShoppingListManager.createShoppingList(newListName);
+                        break;
+                    case TAB_JOIN_INDEX:
+                        String joinCode = joinCodeEditText.getText().toString();
+                        ShoppingListManager.getShoppingList(joinCode, new ListExistsListener() {
+                            @Override
+                            public void onListFound(ShoppingList list) {
+                                utils.getPersistenceManger().persistKnownID(list.getId());
+                                utils.getAdapter().appendItem(list);
+                            }
 
+                            @Override
+                            public void onListNotFound() {
+                                // Show some error
+                            }
+                        });
+                        break;
+                }
             }
         });
 
@@ -64,5 +92,18 @@ public class JoinCreateDialog extends DialogFragment {
         });
 
         return builder.create();
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the NoticeDialogListener so we can send events to the host
+            utils = (ShoppingListUtil) activity;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            throw new ClassCastException(activity.toString() + " must implement ShoppingListUtil");
+        }
     }
 }
